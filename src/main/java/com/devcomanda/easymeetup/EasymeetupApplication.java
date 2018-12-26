79397e5c30044cce0b0d63afba7e6942f377bf18
user.email <d.manzhula@gmail.com>
2018-12-26 21:56:11 +0200

    Initial commit: back end structure created

---
+++ src/main/java/com/devcomanda/easymeetup/EasymeetupApplication.java
@@ -0,0 +1,14 @@
+package com.devcomanda.easymeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymeetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymeetupApplication.class, args);
+	}
+
+}
+
