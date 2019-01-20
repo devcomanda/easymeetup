514b23a96d27ad035acc951f32a0b5ba922a3d8b
alena.kuznetsova <alena.klevtsova@gmail.com>
2019-01-20 12:28:01 +0200

    added router and event page

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Event from  './components/event/Event.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/event/">Event</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/event/" component={Event} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ web/src/components/event/Event.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Event extends Component {
+    render() {
+        return (
+            <h2>Hi from event page</h2>
+        );
+    }
+}
+
+
+export  default  Event;
