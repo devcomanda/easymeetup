a4ee68fba2fcc4e3e8d359f1ba02c97ca132c12a
alena.kuznetsova <alena.klevtsova@gmail.com>
2019-01-26 17:07:15 +0200

    got data from fake db

--- web/src/components/event/Event.js
+++ web/src/components/event/Event.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import EventList from  '../event/EventList';
 
 class Event extends Component {
     render() {
         return (
-            <h2>Hi from event page</h2>
+            <div>
+                <EventList />
+            </div>
         );
     }
 }
-
 
 export  default  Event;
---
+++ web/src/components/event/EventCard.js
@@ -0,0 +1,73 @@
+// @flow
+
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+interface ICardProps {
+    id?: number;
+    title: string;
+    address?: string;
+    date?: string;
+    description: string;
+    speaker?: string;
+}
+
+interface ICardState {
+    image: string;
+    alt: string;
+    title: string;
+}
+
+function EventCard(props): ICardProps {
+    const {
+        id,
+        title,
+        address,
+        date,
+        description,
+        speaker,
+    } = this.props;
+
+    const {
+        image,
+        alt,
+    } = this.state;
+
+    return (
+        <Card>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt={this.props.title}
+                    image="../assets/images/bcg.jpg"
+                    title={this.props.title}
+                />
+                <CardContent>
+                    <Typography>
+                        {this.props.title}
+                    </Typography>
+                    <Typography component="p">
+                        {this.props.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+                <Button size="small" color="primary">
+                    Learn More
+                </Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default EventCard;
+
