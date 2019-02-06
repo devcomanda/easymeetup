2b470d14aa77075ff1684117f14fe4b7fd4b59de
Alona Kuznetsova <akuzne@softserveinc.com>
2019-02-06 16:55:58 +0200

    #46 added typescript

---
+++ web/src/components/event/Event.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EventList from  './EventList';
+
+class Event extends Component {
+    render() {
+        return (
+            <div>
+                <EventList />
+            </div>
+        );
+    }
+}
+
+export  default  Event;
---
+++ web/src/components/event/EventList.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+// import { useState, useEffect } from 'react';
+import axios from 'axios';
+import EventCard from  './EventCard';
+
+
+class EventList extends Component {
+
+    state = {
+        event: []
+    };
+
+    componentDidMount() {
+        axios.get(`http://localhost:3001/event`)
+            .then(res => {
+                const event = res.data;
+                this.setState({ event });
+            })
+    }
+
+
+    render() {
+        return (
+            <ul>
+                { this.state.event.map(event => <EventCard key={event.id}>{...event}</EventCard>)}
+            </ul>
+        );
+    }
+}
+
+export  default  EventList;
