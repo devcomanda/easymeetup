94af9b5a4b467a5f41efaa8a6da09f8c0ab298fd
user.email <d.manzhula@gmail.com>
2019-02-11 22:35:50 +0200

    next test with bugs

--- src/test/java/com/devcomanda/easymeetup/factories/UsersFactory.java
+++ src/test/java/com/devcomanda/easymeetup/factories/UsersFactory.java
@@ -11,6 +11,7 @@ public final class UsersFactory {
     public static final Long FIRST_USER_ID = 1L;
     public static final String FIRST_USER_EMAIL = "email@email.com";
     public static final String FIRST_USER_PLAIN_PASSWORD = "user";
+    public static final Boolean FIRST_USER_ACTIVATED_STATUS = true;
 
     public static User firstUser() {
         User user = new User(FIRST_USER_EMAIL, FIRST_USER_PLAIN_PASSWORD);
