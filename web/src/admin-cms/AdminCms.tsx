59fb4cc2e2b969b08d42d5cbbb232ca929d230b8
Fedichkin Denis <fedichkin.dy@gmail.com>
2019-02-16 20:31:21 +0200

    add right menu and top panel

--- web/src/admin-cms/AdminCms.tsx
+++ web/src/admin-cms/AdminCms.tsx
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
 import "bootstrap/scss/bootstrap.scss";
+import RightMenu from "./RightMenu";
+import HeadPanel from "./HeadPanel";
 
 class AdminCms extends Component {
+
     render() {
+
         return (
             <div className="container-fluid">
+                <div className="row" style={{marginBottom: "15px"}}>
+                    <div className="col-12">
+                        <HeadPanel />
+                    </div>
+                </div>
                 <div className="row">
-                    <div className="col-2">1</div>
+                    <div className="col-2">
+                        <RightMenu/>
+                    </div>
                     <div className="col-10">
-                        <h1>Административная панель</h1>
                     </div>
                 </div>
             </div>
