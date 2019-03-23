6fbac3cfb10d91105e2f88fca527b38ae1884fcf
Alexander <berezkin05@gmail.com>
2019-03-23 23:17:19 +0200

    deleted redundant rows

--- src/test/java/com/devcomanda/easymeetup/controller/UserControllerIntTest.java
+++ src/test/java/com/devcomanda/easymeetup/controller/UserControllerIntTest.java
@@ -1,29 +1,20 @@
 package com.devcomanda.easymeetup.controller;
 
 import com.devcomanda.easymeetup.factories.UsersFactory;
-import com.devcomanda.easymeetup.model.entity.User;
-import com.devcomanda.easymeetup.repository.UserRepository;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 /**
  * @author Alexander Beryozkin
